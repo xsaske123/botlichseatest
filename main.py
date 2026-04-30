7ef0a4e876f636967a217d553721524d72e37162
sebastian <sea.gonzalezb@duocuc.cl>
2026-04-29 20:45:37 -0400

    arreglo de token (pinche dc)

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ import voz
 import tienda #módulo de tienda
 import personajes  # módulo de personajes
 
-TOKEN ="MTQ3ODE3ODA5MTk1OTU4Mjc2MQ.GxKqqQ.9ZH9EnRCNbkQCMfbRU6zuA9EgatyRjnMZytGI4"
+import os
+from dotenv import load_dotenv
+
+TOKEN = os.getenv('DISCORD_TOKEN')
 ID_CANAL_VOZ = 1478183764021481654
 ARCHIVO_DATOS = 'datos_lichsea.json'
 
